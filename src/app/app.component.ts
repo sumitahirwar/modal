98fb231d5eeedea9b2db60c6397feb88f70ff7bc
sumitahirwar <34581600+sumitahirwar@users.noreply.github.com>
2020-01-16 18:36:33 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import{NgbdModalBasic} from './modal-basic';
+
+
+@Component({
+  selector: 'app-root',
+  template:`
+      Hello Angular 
+     < br>
+     <ngbd-modal-basic></ngbd-modal-basic>
+
+  `
+})
+export class AppComponent {
+  title = 'home-loan';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import{NgbdModalBasic} from './modal-basic';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgbdModalBasic
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
